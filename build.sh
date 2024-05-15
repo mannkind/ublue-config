97121c3eb33f86e0969bc180fc8201118ea2e2fb
Dustin Brewer <spry.idea4177@thenullpointer.net>
2024-05-14 22:00:57 -0700

    Install 1Password

--- build.sh
+++ build.sh
@@ -11,9 +11,20 @@ RELEASE="$(rpm -E %fedora)"
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+cat > /etc/yum.repos.d/1password.repo << EOF
+[1password]
+name=1Password Stable Channel
+baseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+#gpgkey="https://downloads.1password.com/linux/keys/1password.asc"
+gpgkey=file:///etc/pki/rpm-gpg/1password.asc
+EOF
+wget https://downloads.1password.com/linux/keys/1password.asc -O /etc/pki/rpm-gpg/1password.asc
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install screen 1password
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
