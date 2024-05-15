7be0d481e8edbdff73e4879e3dd5279f51d32546
Dustin Brewer <spry.idea4177@thenullpointer.net>
2024-05-14 22:09:54 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ name=1Password Stable Channel
 baseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch
 enabled=1
 gpgcheck=1
-repo_gpgcheck=1
+repo_gpgcheck=0
 #gpgkey="https://downloads.1password.com/linux/keys/1password.asc"
 gpgkey=file:///etc/pki/rpm-gpg/1password.asc
 EOF
